e4017a05cf80c02195db924563bf359462a5b738
Florian Mahlknecht <m@florian.world>
2021-03-25 14:38:42 +0100

    Adds a CSV writer template class

---
+++ include/x/common/csv_writer.h
@@ -0,0 +1,106 @@
+//
+// Created by Florian Mahlknecht on 2021-03-25.
+// Copyright (c) 2021 NASA / JPL. All rights reserved.
+
+
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <limits>
+#include <cassert>
+#include <queue>
+#include <tuple>
+
+namespace x {
+
+  /**
+   * Recursive variadic template function which writes the values in CSV format to a file.
+   * @tparam First type to be written on ostream
+   * @tparam Types variadic templates waiting to be written
+   * @param out ostream to work on (e.g. std::err or std::ofstream)
+   * @param first value to be written on ostream
+   * @param values values to be called recursively
+   */
+  template<class First, class... Types>
+  void csv_write_row(std::ostream& out, First &&first, Types &&... values) {
+    out << first;
+    // evaluated at compile time --> C++17
+    if constexpr (sizeof...(Types) > 0) {
+      out << ";";
+      x::csv_write_row(out, values...);
+    } else {
+      out << std::endl;
+    }
+  }
+
+  /**
+   * High performance scientific CSV writer.
+   * Provides a type-safe way to write CSV files through a templated addRow() method. These are written into a
+   * queue to provide superior performance. Flush() writes them correctly formatted into a file.
+   * All types are required to have a "<<" ostream operator.
+   *
+   * // Example usage:
+   * x::CsvWriter<std::string, double, double> csv("test.csv", {"type", "x", "accuracy"});
+   * csv.addRow("IMU", 1.6, 0.000004*(-1));
+   *
+   * @tparam Types types to be used in the
+   */
+  template<class ... Types>
+  class CsvWriter {
+  public:
+    /**
+     * Opens the passed 'filename' as file in write mode and writes the provided column names in the constructor
+     * directly to the file. They are required to have the same length as the variadic template types.
+     * @param filename
+     * @param column_names e.g. { "col1", "col2" }
+     */
+    CsvWriter(const std::string& filename, std::initializer_list<std::string> column_names);
+
+    /**
+     * Writes the buffer in CSV-format to the open file.
+     */
+    void flush() {
+      while (!buffer_.empty()) {
+        // expands variadic template types and forwards them to x::csv_write_row
+        std::apply([=](auto &&... args) { x::csv_write_row(outfile_, args...); }, buffer_.front());
+        buffer_.pop();
+      }
+      outfile_.flush();
+    }
+
+    void addRow(Types &&... values) {
+      buffer_.emplace(values...);
+    }
+
+    ~CsvWriter() {
+      flush();
+      outfile_.close();
+    }
+
+  private:
+    std::queue<std::tuple<Types...>> buffer_;
+    std::ofstream outfile_;
+  };
+
+  template<class... Types>
+  CsvWriter<Types...>::CsvWriter(const std::string &filename, std::initializer_list<std::string> column_names) {
+    assert(column_names.size() == sizeof...(Types));
+    outfile_.open(filename);
+
+    // use double output precision
+    outfile_.flags(std::ios::scientific);
+    outfile_.precision(std::numeric_limits<double>::digits10);
+
+    // place ; after each element except last
+    auto it  = column_names.begin();
+    for (;it < column_names.end()-1; ++it) {
+      outfile_ << *it << ";";
+    }
+    outfile_ << *it << std::endl;
+  }
+
+}
+
+
+
