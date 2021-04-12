e7cb0a237be6c049d68174232603748b554b112f
Friedrich M. Rockenbauer <friedrich.m.rockenbauer@jpl.nasa.gov>
2021-04-12 11:35:38 +0200

    Added lookup tables for keypoints and bearing vectors.

--- include/x/vision/camera.h
+++ include/x/vision/camera.h
@@ -57,6 +57,18 @@ public:
   Track normalize(const Track& track, const size_t max_size = 0) const;
   TrackList normalize(const TrackList& tracks, const size_t max_size = 0) const;
 
+  cv::Point2d project(const Eigen::Vector3d& bearing) const;
+  Eigen::Vector3d backProject(const cv::Point2d& keypoint) const;
+
+  void pinholeProject(Eigen::Vector2d& px) const;
+  void pinholeProject(cv::Point2d& px) const;
+
+  void pinholeBackProject(cv::Point2d& px) const;
+  void pinholeBackProject(Eigen::Vector2d& px) const;
+  void pinholeBackProject(Eigen::Vector3d& bearing) const;
+
+  void distort(const cv::Point2d &input, cv::Point2d &distorted_output) const;
+
 private:
   double fx_ = 0.0; // Focal length
   double fy_ = 0.0;
@@ -71,6 +83,12 @@ private:
   double inv_fy_ = -1;
   double cx_n_   = -1; // Principal point in normalized coordinates
   double cy_n_   = -1;
+
+  void calculateBearingLUT();
+  void calculateKeypointLUT();
+
+  Eigen::Matrix<double, 4, Eigen::Dynamic> bearing_lut_;
+  Eigen::Matrix<double, 2, Eigen::Dynamic> keypoint_lut_;
 };
 }
 
--- src/x/vision/camera.cpp
+++ src/x/vision/camera.cpp
@@ -43,6 +43,9 @@ Camera::Camera(double fx,
   inv_fy_ = 1.0 / fy_;
   cx_n_ = cx_ * inv_fx_;
   cy_n_ = cy_ * inv_fy_;
+
+  calculateBearingLUT();
+  calculateKeypointLUT();
 }
 
 unsigned int Camera::getWidth() const
@@ -155,6 +158,7 @@ Feature Camera::normalize(const Feature& feature) const
 
   return normalized_feature;
 }
+// TODO(frockenb) Port descriptions to header file
 
 /** \brief Normalized the image coordinates for all features in the input track
  *  \param track Track to normalized
@@ -193,4 +197,171 @@ TrackList Camera::normalize(const TrackList& tracks, const size_t max_size) cons
     normalized_tracks[i] = normalize(tracks[i], max_size);
 
   return normalized_tracks;
+}
+
+/** \brief Projects point using basic pinhole camera model.
+ *  \param px Point coordinates
+ */
+void Camera::pinholeProject(Eigen::Vector2d& px) const
+{
+  px[0] = px[0] * fx_ + cx_;
+  px[1] = px[1] * fy_ + cy_;
+}
+
+/** \brief Projects point using basic pinhole camera model.
+ *  \param px Point coordinates
+ */
+void Camera::pinholeProject(cv::Point2d& px) const
+{
+  px.x = px.x * fx_ + cx_;
+  px.y = px.y * fy_ + cy_;
+}
+
+/** \brief Back projects point using basic pinhole camera model.
+ *  \param px Point coordinates
+ */
+void Camera::pinholeBackProject(Eigen::Vector2d& px) const
+{
+  px[0] = (px[0] - cx_) * inv_fx_;
+  px[1] = (px[1] - cy_) * inv_fy_;
+}
+
+/** \brief Back projects point using basic pinhole camera model.
+ *  \param px Point coordinates
+ */
+void Camera::pinholeBackProject(cv::Point2d& px) const
+{
+  px.x = (px.x - cx_) * inv_fx_;
+  px.y = (px.y - cy_) * inv_fy_;
+}
+
+/** \brief Back projects bearing vector using basic pinhole camera model.
+ *  \param bearing Bearing vector
+ */
+void Camera::pinholeBackProject(Eigen::Vector3d& bearing) const
+{
+  bearing[0] = (bearing[0] - cx_) * inv_fx_;
+  bearing[1] = (bearing[1] - cy_) * inv_fy_;
+}
+
+/** \brief Unit coordinates -> distortion -> pinhole, offset and scale. // TODO(frockenb) write proper description.
+ *  \param bearing_out Bearing vector
+ *  \return px
+ */
+cv::Point2d Camera::project(const Eigen::Vector3d& bearing) const
+{
+  cv::Point2d px;
+  px.x = bearing[0] / bearing[2];
+  px.y = bearing[1] / bearing[2];
+
+  Camera::distort(px, px);
+  Camera::pinholeProject(px);
+  return px;
+}
+
+/** \brief  // TODO(frockenb) write proper description.
+ *  \param keypoint
+ *  \return bearing_out Bearing vector
+ */
+Eigen::Vector3d Camera::backProject(const cv::Point2d& keypoint) const
+{
+  cv::Point2d keypoint_out = keypoint;
+
+  Camera::pinholeBackProject(keypoint_out);
+  Camera::undistort(keypoint_out, keypoint_out);
+
+  Eigen::Vector3d bearing_out;
+  bearing_out << keypoint_out.x, keypoint_out.y, 1.0;
+
+  return bearing_out.normalized();
+}
+
+/** \brief Precomputes a Look Up Table to convert image coordinates to bearing vectors.
+ *  \pre Image and distortion parameters have to be set prior to bearing_lut_ calculation.
+ */
+void Camera::calculateBearingLUT()
+{
+  size_t n = img_height_ * img_width_;
+  bearing_lut_.resize(4, n);
+
+  cv::Point2d keypoint;
+
+  for (size_t y=0; y != img_height_; ++y)
+  {
+    for (size_t x=0; x != img_width_; ++x)
+    {
+      keypoint.x = (double) x;
+      keypoint.y = (double) y;
+      Eigen::Vector3d f = backProject(keypoint);
+      bearing_lut_.col(x + y * img_width_) =
+        Eigen::Vector4d(f[0], f[1], f[2], 1.);
+    }
+  }
+}
+
+/** \brief Precomputes a Look Up Table to convert bearing vectors to image coordinates.
+ *  \pre Image and distortion parameters have to be set prior to keypoint_lut_ calculation.
+ *  \pre bearing_lit_ has to be computed prior to keypoint_lut_ calculation.
+ */
+void Camera::calculateKeypointLUT()
+{
+  size_t n = img_height_ * img_width_;
+  keypoint_lut_.resize(2, n);
+
+  for (size_t i=0; i != n; ++i)
+  {
+    cv::Point2d p = project(bearing_lut_.col(i).head<3>().cast<double>());
+
+    Eigen::Vector2d keypoint;
+    keypoint[0] = p.x;
+    keypoint[1] = p.y;
+
+    keypoint_lut_.col(i) = keypoint;
+  }
+}
+
+void Camera::distort(const cv::Point2d &input, cv::Point2d &distorted_output) const
+{
+  const double x = input.x;
+  const double y = input.y;
+
+  const double xx = x * x;
+  const double yy = y * y;
+  const double r_sq = xx + yy;
+
+  switch(distortion_model_) {
+    case DistortionModel::FOV: {
+      const double& s = distortion_parameters_[0];
+      const double s_term = ( 2.0 * std::tan(s / 2.0) );
+      const double rad = std::sqrt(r_sq);
+      const double factor = (rad < 0.001) ? 1.0 : std::atan(rad * s_term) / (s * rad);
+
+      distorted_output.x = x * factor;
+      distorted_output.y = y * factor;
+
+      break;
+      }
+    case DistortionModel::RADIAL_TANGENTIAL: {
+      // @see https://github.com/ethz-asl/image_undistort/blob/master/src/undistorter.cpp
+      // Split out parameters for easier reading
+      const double &k1 = distortion_parameters_[0];
+      const double &k2 = distortion_parameters_[1];
+      //const double &k3 = distortion_parameters_[4];
+      const double &p1 = distortion_parameters_[2];
+      const double &p2 = distortion_parameters_[3];
+
+      const double xy = x * y;
+      const double cdist = (k1 + k2 * r_sq) * r_sq;
+
+      distorted_output.x = x + (x * cdist + p1 * 2.0 * xy + p2 * (r_sq + 2.0 * xx));
+      distorted_output.y = y + (y * cdist + p2 * 2.0 * xy + p1 * (r_sq + 2.0 * yy));
+
+      break;
+    }
+    default: {
+      std::ostringstream message;
+      message << "Distortion model not implemented - model: " << static_cast<int>(distortion_model_);
+      throw std::runtime_error(message.str());
+    }
+  }
 }
