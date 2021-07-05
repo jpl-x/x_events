9346ba9d0321369d968d95729a20a455ab6e5146
Florian Mahlknecht <m@florian.world>
2021-07-05 12:57:03 +0200

    Adds HASTE source code to module, while just modifying the include paths to <x/haste/...>

---
+++ src/x/haste/tracking/tracking.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2021 by Ignacio Alzugaray <alzugaray dot ign at gmail dot com>
+// ETH Zurich, Vision for Robotics Lab.
+
+#include <x/haste/tracking/correlation_tracker.hpp>
+#include <x/haste/tracking/haste_correlation_tracker.hpp>
+#include <x/haste/tracking/haste_correlation_star_tracker.hpp>
+#include <x/haste/tracking/haste_difference_tracker.hpp>
+#include <x/haste/tracking/haste_difference_star_tracker.hpp>
